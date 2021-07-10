66a26111f7505d8d297d4994c7f9904b1d4c7ea7
Tan Yi Peng <tanyipeng834@gmail.com>
2021-07-10 13:05:02 +0800

    Finished compareTo method and tested

--- Point.java
+++ Point.java
@@ -1,11 +1,166 @@
+/******************************************************************************
+ *  Compilation:  javac Point.java
+ *  Execution:    java Point
+ *  Dependencies: none
+ *  
+ *  An immutable data type for points in the plane.
+ *  For use on Coursera, Algorithms Part I programming assignment.
+ *
+ ******************************************************************************/
+
+import java.util.Comparator;
+import edu.princeton.cs.algs4.StdDraw;
+
+
+
 public class Point implements Comparable<Point> {
-    public Point(int x, int y)                         // constructs the point (x, y)
- 
-    public   void draw()                               // draws this point
-    public   void drawTo(Point that)                   // draws the line segment from this point to that point
-    public String toString()                           // string representation
- 
-    public               int compareTo(Point that)     // compare two points by y-coordinates, breaking ties by x-coordinates
-    public            double slopeTo(Point that)       // the slope between this point and that point
-    public Comparator<Point> slopeOrder()              // compare two points by slopes they make with this point
- }
+
+    private final int x;     // x-coordinate of this point
+    private final int y;     // y-coordinate of this point
+
+    /**
+     * Initializes a new point.
+     *
+     * @param  x the <em>x</em>-coordinate of the point
+     * @param  y the <em>y</em>-coordinate of the point
+     */
+    public Point(int x, int y) {
+        /* DO NOT MODIFY */
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * Draws this point to standard draw.
+     */
+    public void draw() {
+        /* DO NOT MODIFY */
+        StdDraw.point(x, y);
+    }
+
+    /**
+     * Draws the line segment between this point and the specified point
+     * to standard draw.
+     *
+     * @param that the other point
+     */
+    public void drawTo(Point that) {
+        /* DO NOT MODIFY */
+        StdDraw.line(this.x, this.y, that.x, that.y);
+    }
+
+    /**
+     * Returns the slope between this point and the specified point.
+     * Formally, if the two points are (x0, y0) and (x1, y1), then the slope
+     * is (y1 - y0) / (x1 - x0). For completeness, the slope is defined to be
+     * +0.0 if the line segment connecting the two points is horizontal;
+     * Double.POSITIVE_INFINITY if the line segment is vertical;
+     * and Double.NEGATIVE_INFINITY if (x0, y0) and (x1, y1) are equal.
+     *
+     * @param  that the other point
+     * @return the slope between this point and the specified point
+     */
+    public double slopeTo(Point that) {
+       
+        /* YOUR CODE HERE */
+        return 0;
+    }
+
+    /**
+     * Compares two points by y-coordinate, breaking ties by x-coordinate.
+     * Formally, the invoking point (x0, y0) is less than the argument point
+     * (x1, y1) if and only if either y0 < y1 or if y0 = y1 and x0 < x1.
+     *
+     * @param  that the other point
+     * @return the value <tt>0</tt> if this point is equal to the argument
+     *         point (x0 = x1 and y0 = y1);
+     *         a negative integer if this point is less than the argument
+     *         point; and a positive integer if this point is greater than the
+     *         argument point
+     */
+    public int compareTo(Point that) {
+        /* YOUR CODE HERE */
+        // compare by the y coordinates first
+        if (this.y>that.y){
+            return +1;
+
+        }
+        // assuiming there is a tie of the y coordinates of the point, have to break by the x cooridnates
+        if (this.y== that.y){
+
+            // check if the x coordinates is larger
+            if(this.x>that.x){
+                return +1;
+
+            }
+            else if (this.x==that.x){
+                return 0;
+            }
+            // if the x coordinates is smaller ,then means it is smaller
+            else{
+                return -1;
+            }
+        
+        }
+        // when the other Point y cooridantes is larger than the current point
+        else{
+            return -1;
+        }
+
+    }
+
+    /**
+     * Compares two points by the slope they make with this point.
+     * The slope is defined as in the slopeTo() method.
+     *
+     * @return the Comparator that defines this ordering on points
+     */
+    public Comparator<Point> slopeOrder() {
+        return null;
+        /* YOUR CODE HERE */
+    }
+
+
+    /**
+     * Returns a string representation of this point.
+     * This method is provide for debugging;
+     * your program should not rely on the format of the string representation.
+     *
+     * @return a string representation of this point
+     */
+    public String toString() {
+        /* DO NOT MODIFY */
+        return "(" + x + ", " + y + ")";
+    }
+
+    
+        
+    
+
+
+
+    
+
+    /**
+     * Unit tests the Point data type.
+     */
+
+
+    
+    public static void main(String[] args) {
+        /* Create Points that are used for testing of the class Methods*/
+        Point p1 = new Point(1,1);
+        Point p2 = new Point(1,2);
+        Point p3 = new Point(3,1);
+
+
+        // Test if the y cooordinates compare points by the y - coordinates
+        assert(p1.compareTo(p2)<0);
+        // Test if the compare to method is able to break by the x - coordinates.
+        assert(p3.compareTo(p1)>0);
+
+    
+        
+        
+    }
+}
